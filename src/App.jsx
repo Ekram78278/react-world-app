8b874418119bc9ad5a1634f6aafd6b739ef7e55e
ekram <ekram78278@gmail.com>
2025-10-03 17:01:34 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Countries from './components/countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+      <Countries></Countries>
+      <h1>in the country</h1>
+   
+      
+    </>
+  )
+}
+
+export default App
