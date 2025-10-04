f8442ca518e44301c6551803a6ec106c8d2f1482
ekram <ekram78278@gmail.com>
2025-10-04 09:35:59 +0600

    2nd commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 
+import { Suspense } from 'react'
 import './App.css'
 import Countries from './components/countries/Countries'
+
+
+const countriesPromise =fetch('https://openapi.programming-hero.com/api/all')
+.then(res => res.json())
+
+
 
 function App() {
   
 
   return (
     <>
-      <Countries></Countries>
+      <Suspense fallback={<p>Nadir vai loading...</p>}  >
+        <Countries countriesPromise={countriesPromise} ></Countries>
+      </Suspense>
       <h1>in the country</h1>
    
       
--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { use } from 'react';
+import Country from '../country/Country';
 
-const Countries = () => {
+const Countries = ({countriesPromise}) => {
+    
+    const countriesData  = use(countriesPromise)
+        
+    const countries = countriesData.countries
+
+    console.log(countries)
+    
     return (
         <div>
-            <h1> Help me out </h1>
+            <h1>In the Countries: {countries.length}</h1>
+        {
+            countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+        }
+
         </div>
+
     );
 };
 
