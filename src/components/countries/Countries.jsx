6d7bd1bd02abd61b89ed1e35c3379425eecb16e6
ekram <ekram78278@gmail.com>
2025-10-04 10:32:05 +0600

    Added button interactivity commit

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,24 +1,27 @@
-import React, { use } from 'react';
-import Country from '../country/Country';
+import { use } from "react";
+import Country from "../country/Country";
+import "./Countries.css";
 
-const Countries = ({countriesPromise}) => {
-    
-    const countriesData  = use(countriesPromise)
-        
-    const countries = countriesData.countries
+const Countries = ({ countriesPromise }) => {
+  const countriesData = use(countriesPromise);
 
-    console.log(countries)
-    
-    return (
-        <div>
-            <h1>In the Countries: {countries.length}</h1>
-        {
-            countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-        }
+  const countries = countriesData.countries;
 
-        </div>
+  console.log(countries);
 
-    );
+  return (
+    <div>
+      <h1>In the Countries: {countries.length}</h1>
+      <div className="countries">
+        {countries.map((country) => (
+          <Country key={country.cca3.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Countries;
+
+// inline css style object
+//
